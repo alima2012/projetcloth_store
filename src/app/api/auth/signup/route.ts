b626b399ac9a5b8926396d7b9321935905c0f747
alimatou sow <a.s33@zig.univ.sn>
2025-03-12 08:12:02 +0000

    modification backend

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -1,47 +1,47 @@
-import bcrypt from "bcryptjs";
-import { NextResponse } from "next/server";
-import User from '@/models/user'
-import connectToDatabase from "@/lib/mongodb";
+// import bcrypt from "bcryptjs";
+// import { NextResponse } from "next/server";
+// import User from '@/models/user'
+// import connectToDatabase from "@/lib/mongodb";
 
-export async function POST(request: Request){
-    const {name, email, password, confirmPassword} = await request.json();
+// export async function POST(request: Request){
+//     const {name, email, password, confirmPassword} = await request.json();
 
-    const isValidEmail = (email: string) => {
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return emailRegex.test(email);
-    }
-    if(!name || !email || !password || !confirmPassword){
-        return NextResponse.json({massage: "Tous les champs sont requis"}, {status:400})
-    }
-    if(!isValidEmail(email)){
-        return NextResponse.json({message: "email invalide"}, {status: 400})
-    }
-    if(confirmPassword !== password){
-        return NextResponse.json({message: "Non de passe non conforme"}, {status: 400})
-    }
-    if(password.length < 6){
-        NextResponse.json({messaage: "Le mot de passe doit contenir au moins 6 caractères"}, {status: 400})
-    }
-    try{
-        await connectToDatabase();
-        const existingUser = await User.findOne({email});
-        if(existingUser){
-            return NextResponse.json({message: "Cet utilisateur existe"}, {status:400})
-        }
+//     const isValidEmail = (email: string) => {
+//         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//         return emailRegex.test(email);
+//     }
+//     if(!name || !email || !password || !confirmPassword){
+//         return NextResponse.json({massage: "Tous les champs sont requis"}, {status:400})
+//     }
+//     if(!isValidEmail(email)){
+//         return NextResponse.json({message: "email invalide"}, {status: 400})
+//     }
+//     if(confirmPassword !== password){
+//         return NextResponse.json({message: "Non de passe non conforme"}, {status: 400})
+//     }
+//     if(password.length < 6){
+//         NextResponse.json({messaage: "Le mot de passe doit contenir au moins 6 caractères"}, {status: 400})
+//     }
+//     try{
+//         await connectToDatabase();
+//         const existingUser = await User.findOne({email});
+//         if(existingUser){
+//             return NextResponse.json({message: "Cet utilisateur existe"}, {status:400})
+//         }
 
-        const hashedPassword = await bcrypt.hash(password,10);
+//         const hashedPassword = await bcrypt.hash(password,10);
 
-        const newUser = new User({
-            email,
-            name,
-            password: hashedPassword,
-        });
-        await newUser.save();
-        return NextResponse.json({message: "Uilisateur créé"}, {status:201});
-    }
-    catch(error){
-        console.log(error)
-        return NextResponse.json({message: "Erreur"}, {status: 500});
+//         const newUser = new User({
+//             email,
+//             name,
+//             password: hashedPassword,
+//         });
+//         await newUser.save();
+//         return NextResponse.json({message: "Uilisateur créé"}, {status:201});
+//     }
+//     catch(error){
+//         console.log(error)
+//         return NextResponse.json({message: "Erreur"}, {status: 500});
 
-    }
-}
+//     }
+// }
