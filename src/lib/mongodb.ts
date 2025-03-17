1c3bfa6794fbf844956b68ba465be5cd28130d22
alimatou sow <a.s33@zig.univ.sn>
2025-03-17 12:46:52 +0000

    ajoutproduit + paiment

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,18 +1,21 @@
 import mongoose from 'mongoose'
 
-const MONGODB_URI = process.env.MONGO;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/BlogProduit";
 
-if(!MONGODB_URI){
-    throw new Error ("définir l'environnement mongodb")
+if (!MONGODB_URI) {
+  throw new Error("définir l'environnement mongodb");
 }
-async function connectToDatabase(){
-    if(mongoose.connection.readyState === 1){
-        return mongoose;
-    }
-    const opts = {
-        bufferCommands: false,
-    }
-    await mongoose.connect(MONGODB_URI!, opts);
-    return mongoose;
+
+let cached = (global as any).mongoose || { conn: null, promise: null };
+
+async function connectToDatabase() {
+  if (cached.conn) return cached.conn;
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => mongoose);
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
+
 export default connectToDatabase;
+
